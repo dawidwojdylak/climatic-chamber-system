89f556c7b855f0b8cd1fabc16460c16dcdc89e63
446UXR <Dawid.Wojdylak@merit-automotive.com>
2023-01-07 19:09:38 +0100

    Improve wifi_task

--- SERVER/wifitask.sh
+++ SERVER/wifitask.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-
-printf "\n\n************************************************************************\n" >> /home/dawid/wifi_task/wifi_log.txt
-date >> /home/dawid/wifi_task/wifi_log.txt
-
-if [[ "$(ping -c 1 8.8.8.8 | grep '100% packet loss' )" != "" ]]; then
-    printf "The Internect connection is not active.\nReseting...\n" >> /home/dawid/wifi_task/wifi_log.txt
+printf "\n\n************************************************************************\n" >> $(pwd)/wifi_log.txt
+date >> $(pwd)/wifi_log.txt
+if [ ping -q -c1 8.8.8.8 &>/dev/null ]; then
+    printf "The Internect connection is not active.\nReseting...\n" >> $(pwd)/wifi_log.txt
 
     nmcli networking off
     nmcli networking on
     
     nmcli -o >> wifi_log
    
-    # exit 1
 else
-    printf "Internet connection is active.\n" >> /home/dawid/wifi_task/wifi_log.txt
+    printf "Internet connection is active.\n" >> $(pwd)/wifi_log.txt
 fi
