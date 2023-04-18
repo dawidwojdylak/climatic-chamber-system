d79bc565362658355df9310e5cac68523070e87a
dawidwojdylak <dawidwojdylak98@gmail.com>
2023-04-18 12:12:52 +0200

    Add thesis

--- Tests/ssh_cms_conn.py
+++
@@ -1,7 +0,0 @@
-import paramiko
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # niebezpieczne, weryfikowac manualnie
-ssh.connect('10.224.157.83', username='test', password='test')
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("uptime")
-print(ssh_stdout.readlines())
